d38cd9aa8b571a22b0ca8bb865df98da1c661304
Dmitry <st1makk@gmail.com>
2023-04-24 19:43:15 +0700

    19zadacha_samosto9telna9

--- task19/Program.cs
+++ task19/Program.cs
@@ -6,48 +6,33 @@
 // 12821 -> да
 // 23432 -> да
 
-int Promt(string message)
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99999 || number < 10000)
 {
-    System.Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
+    Console.WriteLine("Введенное число не 5 значное");
+    return;
 }
 
-bool CheckNumberLength(int num)
+Console.WriteLine(PalindromeCheck(number));
+
+
+bool PalindromeCheck(int num)
 {
-    if (num >= 10000 && num < 100000)
+    if (num / 10000 == num % 10)
     {
-        return true;
+        int firstDigit = num / 1000 % 10;
+        int secondDigit = num / 10 % 10;
+        if (firstDigit == secondDigit)
+        {
+            Console.WriteLine("Число палиндром");
+            return true;
+        }
+        else Console.WriteLine("число не палиндром");
+        return false;
     }
-    Console.WriteLine("Your number contains more or less then 5 digits.");
+    else Console.WriteLine("число не палиндром");
     return false;
-}
-
-bool CheckPalindrome(int numb)
-{
-    int length = numb.ToString().Length;
-    int[] array = new int[length];
-    int tempNumber = numb;
-    int revNumb = 0;
-    for (int i = 0; i < length; i++)
-    {
-        int reminder = tempNumber % 10;
-        array[i] = reminder;
-        tempNumber = tempNumber / 10;
-        revNumb += array[i] * Convert.ToInt32(Math.Pow(10, length - i - 1));
-    }
-    if (revNumb == numb)
-    {
-        Console.WriteLine("Your number is a palendrome");
-        return true;
-    }
-    Console.WriteLine("Your number isn't a palendrome");
-    return false;
-}
-
-int number = Promt("Please enter five-digit number: ");
-if (CheckNumberLength(number))
-{
-    Console.WriteLine(CheckPalindrome(number));
 }
